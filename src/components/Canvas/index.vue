74612f182ac9a0bcd70f6babaf35a332fa0836af
AvailableForTheWorld <435829792@qq.com>
2022-05-01 22:40:48 +0800

    回退到第一版

--- src/components/Canvas/index.vue
+++ src/components/Canvas/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="canvas-container" ref="container">
-    <canvas id="canvas" ref="canvas" @click="showInput($event)" ></canvas>
+    <canvas id="canvas" ref="canvas" @click="showEdit($event)" ></canvas>
     <input type="text" ref="Input" @keyup.enter="storeText()"/>
     <div class="words-container" ref="wordsContainer"></div>
 </div>
@@ -25,8 +25,45 @@ const posX=ref(0),posY=ref(0);
 let isFocus = false;
 let isSelectWords = false;
 
+
+
+const showEdit = (e)=>{
+    console.log("bool",isSelect.value.bool)
+    if(isSelect.value.bool){
+        if(isSelect.value.index===4){
+            showInput(e);
+        }
+        else if(isSelect.value.index===3){
+            showPaint(e);
+        }
+    }
+}
+
+const handleLineMove = (e)=>{
+    e.target.addEventListener('mouseup',handleLineUp);
+
+}
+
+const handleLineUp = (e)=>{
+    context.value.lineTo(e.clientX,e.clientY);
+    context.value.strokeStyle = 'green';
+    context.value.lineWidth = 5;
+    context.value.stroke();
+    context.value.closePath();
+    e.target.removeEventListener('mousemove',handleLineMove);
+}
+
+const showPaint = (e)=>{
+    console.log("e.client",e.clientX,e.clientY)
+    context?.value?.beginPath();
+    context.value.moveTo(e.clientX,e.clientY);
+    
+    e.target.addEventListener('mousemove',handleLineMove)
+}
+
+
 const showInput = (e)=>{
-    if(!isSelect.value.bool||isSelect.value.index!==4||isSelectWords){
+    if(isSelectWords){
         return;
     }
     if(isFocus){
@@ -107,9 +144,11 @@ const editInput = (e)=>{
 
 onMounted(()=>{
     context.value = canvas.value.getContext('2d')
+    canvas.value.width = document.body.clientWidth;
+    canvas.value.height = document.body.clientHeight;
+    // console.log("context.value.width",context.value)
     // devicePixelRatio.value = window.devicePixelRatio;
-    context.value.width = context.value.width * 8;
-    context.value.height = context.value.height * 8;
+    
     context.value.font = 12+'px 微软雅黑'
     // makeHighRes(context.value);
 })
@@ -121,10 +160,7 @@ onMounted(()=>{
     width: 100vw;
     height: 100vh;
     background-color: #eee;
-    #canvas{
-        width: 100%;
-        height: 100%;
-    }
+    overflow: hidden;
     input{
         position: absolute;
         top: 0;
